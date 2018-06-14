22b7c73578d08f8e8ca065594b16a24b525a4907
Oscar Wolter <Oscardawolter@gmail.com>
2018-06-14 14:36:37 +0200

    Started work on the lab

--- Webapp-LAB4/Webapp-LAB4/ClientApp/routes.tsx
+++ Webapp-LAB4/Webapp-LAB4/ClientApp/routes.tsx
@@ -3,10 +3,9 @@ import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import Home from './components/Home';
 import FetchData from './components/FetchData';
-import Counter from './components/Counter';
+import Highscore from './components/Highscore';
 
 export const routes = <Layout>
     <Route exact path='/' component={ Home } />
-    <Route path='/counter' component={ Counter } />
-    <Route path='/fetchdata/:startDateIndex?' component={ FetchData } />
+    <Route path='/highscore' component={ Highscore } />
 </Layout>;
---
+++ Webapp-LAB4/Webapp-LAB4/Models/DB_Models.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webapp_LAB4.Models
+{
+    public class QuestionClass
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string CorrectAwser { get; set; }
+        public string WrongAwser1 { get; set; }
+        public string WrongAwser2 { get; set; }
+        public string WrongAwser3 { get; set; }
+
+    }
+    public class UserClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public int Highscore { get; set; }
+
+    }
+}
